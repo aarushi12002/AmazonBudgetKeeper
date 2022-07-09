1c0efa4f4ce4798914a92828041f797af0de7b05
aarushi <aarushi12002@iiitd.ac.in>
2022-07-09 11:38:05 -0700

    "added from and to fields to define the length of the time in which money was spent"

--- background.js
+++ background.js
@@ -20,9 +20,25 @@ function getOrderSumOnPage() {
   console.log("Executing getOrder");
   x= document.getElementById("ordersContainer");
   y = x.getElementsByClassName("a-box-group a-spacing-base order js-order-card");
+  console.log(y);
   let total = 0;
   for (let i = 0; i < y.length; i++) {
     z=y[i].getElementsByClassName("a-column a-span2 yohtmlc-order-total");
+
+
+    if (i==0) {
+
+         from =  y[0].getElementsByClassName("a-color-secondary value")[0].innerHTML;
+         chrome.storage.sync.set({'From': from}, function() {
+                      console.log('Value is set for from');
+            });
+    } else if(i==y.length-1) {
+        to =  y[i].getElementsByClassName("a-color-secondary value")[0].innerHTML;
+         chrome.storage.sync.set({'To': to}, function() {
+                      console.log('Value is set for To');
+            });
+    }
+
     u=z[0];
     m = u.outerText.split(/\r?\n/)[1].split('$')[1];
     total+= parseInt(m);
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,7 +1,17 @@
 
 let moneyPlaceholder = document.getElementById("money");
+let from = document.getElementById("From");
+let to = document.getElementById("To");
 
 chrome.storage.sync.get("moneyAmount", function(result)  {
   moneyPlaceholder.innerHTML = result.moneyAmount;
   console.log(document.getElementById("money"));
+});
+
+chrome.storage.sync.get("From", function(result)  {
+  from.innerHTML = "From: " + result.From;
+});
+
+chrome.storage.sync.get("To", function(result)  {
+  to.innerHTML = "To:" + result.To;
 });
